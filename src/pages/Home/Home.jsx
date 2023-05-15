2981bef999feb94c0e25965c74e7b9d69dcaa598
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2023-05-15 18:29:50 +0600

    added content card section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import ContentCard from '../ContentCard/ContentCard';
+import SearchBar from './SearchBar/SearchBar';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <SearchBar></SearchBar>
+            <ContentCard></ContentCard>
         </div>
     );
 };

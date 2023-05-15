c6b437386f7e5e7c34635fa3b940bc7e1214cc36
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2023-05-15 22:13:50 +0600

    remove overlow

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { routers } from './routers/Router';
 
 function App() {
   return (
-    <div className='max-w-screen-2xl mx-auto bg-black text-white'>
+    <div className='max-w-screen-2xl mx-auto bg-black text-white overflow-hidden'>
       <RouterProvider router={routers}>
       </RouterProvider>
     </div>
